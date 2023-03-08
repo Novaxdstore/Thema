5361d852bf403069615529f695f108b135ad58b8
Novaxdstore <127206908+Novaxdstore@users.noreply.github.com>
2023-03-08 15:41:06 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@/components/App';
 import { setConfig } from 'react-hot-loader';
-import './NovaxdstoreThema.css';
+import './MinecraftPurpleTheme.css';
 
 // Enable language support.
 import './i18n';
